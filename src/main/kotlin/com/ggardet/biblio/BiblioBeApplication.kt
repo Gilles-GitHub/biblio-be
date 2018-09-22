514fc60a8c1147b033e3cd2e01f2ef3df0f9d99f
Gilles <ggardet@hotmail.fr>
2018-09-22 22:34:30 +0200

    Initial commit

---
+++ src/main/kotlin/com/ggardet/biblio/BiblioBeApplication.kt
@@ -0,0 +1,11 @@
+package com.ggardet.biblio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BiblioBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BiblioBeApplication>(*args)
+}
