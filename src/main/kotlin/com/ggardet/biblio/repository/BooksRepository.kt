4bda0e1964761ba11cca4819e82802bf5a8fb4fa
GARDET GILLES <gigardet-ext@airfrance.fr>
2018-09-28 11:13:29 +0200

    - add context path - trivial stuff

--- src/main/kotlin/com/ggardet/biblio/repository/BooksRepository.kt
+++ src/main/kotlin/com/ggardet/biblio/repository/BooksRepository.kt
@@ -3,4 +3,6 @@ package com.ggardet.biblio.repository
 import com.ggardet.biblio.model.Book
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface BooksRepository : MongoRepository<Book, String>
+interface BooksRepository : MongoRepository<Book, String> {
+    override fun findAll(): List<Book>
+}
