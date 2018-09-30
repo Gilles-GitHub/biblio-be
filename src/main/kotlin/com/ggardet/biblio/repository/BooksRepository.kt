7c6da9caf7c7cce7e6a7420ded150075b2b03059
Gilles <ggardet@hotmail.fr>
2018-09-30 16:20:20 +0200

    - change application.properties to yml file - add doc to the seocnd endpoint - remove acces directly from the repository(it will be done through the controller instead)

--- src/main/kotlin/com/ggardet/biblio/repository/BooksRepository.kt
+++ src/main/kotlin/com/ggardet/biblio/repository/BooksRepository.kt
@@ -3,5 +3,4 @@ package com.ggardet.biblio.repository
 import com.ggardet.biblio.model.Book
 import org.springframework.data.mongodb.repository.MongoRepository
 
-//@RepositoryRestResource(collectionResourceRel = "books", path = "books")
 interface BooksRepository : MongoRepository<Book, String>
