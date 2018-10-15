61708c0878c35eb43d2b2f2268805f68f4c680fd
Gilles <ggardet@hotmail.fr>
2018-10-15 12:01:53 +0200

    - config seems to be good - still need to create UUIDAuthenticationService

--- src/main/kotlin/com/ggardet/biblio/security/config/TokenAuthenticationFilter.kt
+++ src/main/kotlin/com/ggardet/biblio/security/config/TokenAuthenticationFilter.kt
@@ -1,32 +1,37 @@
 package com.ggardet.biblio.security.config
 
 import com.google.common.net.HttpHeaders
-import org.apache.commons.lang3.StringUtils
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
 import org.springframework.security.web.util.matcher.RequestMatcher
+import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-
-class TokenAuthenticationFilter: AbstractAuthenticationProcessingFilter {
+/**
+ * The TokenAuthenticationFilter is responsible of extracting the authentication token from the request headers
+ * It takes the Authorization header value and attempts to extract the token from it
+ */
+class TokenAuthenticationFilter : AbstractAuthenticationProcessingFilter {
 
     private val BEARER = "Bearer"
 
     constructor(requiresAuthenticationRequestMatcher: RequestMatcher?) : super(requiresAuthenticationRequestMatcher)
 
     override fun attemptAuthentication(request: HttpServletRequest?, response: HttpServletResponse?): Authentication {
-        val param = request?.getHeader((HttpHeaders.AUTHORIZATION))?: request?.getParameter("t")
-        val token = StringUtils.removeStart(param, BEARER)
-
-
-//        val token = ofNullable(param)
-//                .map({ value -> removeStart(value, BEARER) })
-//                .map(???({ it.trim({ it <= ' ' }) }))
-//        .orElseThrow { BadCredentialsException("Missing Authentication Token") }
+        val param = request?.getHeader((HttpHeaders.AUTHORIZATION)) ?: request?.getParameter("t")
+        val token = param?.removeSuffix(BEARER)?.trim() ?: throw BadCredentialsException("Missing Authentication Token")
 
         val auth = UsernamePasswordAuthenticationToken(token, token)
+
         return authenticationManager.authenticate(auth)
     }
+
+    override fun successfulAuthentication(request: HttpServletRequest?, response: HttpServletResponse?, chain: FilterChain?, authResult: Authentication?) {
+        super.successfulAuthentication(request, response, chain, authResult)
+        chain!!.doFilter(request, response)
+    }
+
 }
