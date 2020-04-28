443aebfe0b04bccc0b5b4ac060cdbb6e5cc6afd8
Gilles <ggardet@hotmail.fr>
2020-04-28 21:17:02 +0200

    - add post, put and delete methods to the controller

--- src/main/kotlin/com/ggardet/biblio/controller/BookController.kt
+++ src/main/kotlin/com/ggardet/biblio/controller/BookController.kt
@@ -8,10 +8,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @CrossOrigin(origins = ["http://localhost:4200"], maxAge = 3600)
@@ -24,20 +21,50 @@ class BookController(var bookRepository: BookRepository) {
     @GetMapping("/books")
     fun getBooks(): ResponseEntity<List<BookEntity>> {
         logger.info("Provide all books")
-        val response: List<BookEntity> = bookRepository.findAll()
+        val responses: List<BookEntity> = bookRepository.findAll()
         // add hateoas links for every book of the collection (get one)
-        response.forEach { book -> book.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController::class.java).getBook(book.id)).withRel("book")) }
-        response.forEach { book -> book.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController::class.java).getBook(book.id)).withSelfRel()) }
-        return ResponseEntity.ok(response)
+        responses.forEach { book -> book.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController::class.java).getBook(book.id)).withRel("book")) }
+        responses.forEach { book -> book.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController::class.java).getBook(book.id)).withSelfRel()) }
+        return ResponseEntity.ok(responses)
     }
 
     @ApiResponses(value = [ApiResponse(responseCode = "200", description = "Finds the book from its code identifier")])
     @GetMapping("/books/{id}")
     fun getBook(@PathVariable("id") id: String): ResponseEntity<Optional<BookEntity>> {
         logger.info("Provide a book")
-        val book: Optional<BookEntity> = bookRepository.findById(id)
-        book.get().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController::class.java).getBook(book.get().id)).withRel("book"))
-        return ResponseEntity.ok(book)
+        val response: Optional<BookEntity> = bookRepository.findById(id)
+        // add hateoas link
+        response.get().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController::class.java).getBook(response.get().id)).withRel("book"))
+        return ResponseEntity.ok(response)
+    }
+
+    @ApiResponses(value = [ApiResponse(responseCode = "200", description = "Create a book")])
+    @PostMapping("/books")
+    fun postBook(@RequestBody request: BookEntity): ResponseEntity<BookEntity> {
+        logger.info("Create a book")
+        val response = bookRepository.save(request)
+        // add hateoas link
+        response.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController::class.java).getBook(response.id)).withRel("book"))
+        return ResponseEntity.ok(response)
+    }
+
+    @ApiResponses(value = [ApiResponse(responseCode = "204", description = "Update a book find from its identifier")])
+    @PutMapping("/books/{id}")
+    fun putBook(@PathVariable("id") id: String, @RequestBody request: BookEntity): ResponseEntity.HeadersBuilder<*> {
+        logger.info("Update a book")
+        val response = bookRepository.save(request)
+        // add hateoas link
+        response.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(BookController::class.java).getBook(response.id)).withRel("book"))
+        return ResponseEntity.noContent()
+    }
+
+    @ApiResponses(value = [ApiResponse(responseCode = "204", description = "Delete a book from its code identifier")])
+    @DeleteMapping("/books/{id}")
+    fun deleteBook(@PathVariable("id") id: String): ResponseEntity.HeadersBuilder<*> {
+        logger.info("Delete a book")
+        val bookEntity = bookRepository.findById(id)
+        bookRepository.delete(bookEntity.get())
+        return ResponseEntity.noContent()
     }
 
 }
