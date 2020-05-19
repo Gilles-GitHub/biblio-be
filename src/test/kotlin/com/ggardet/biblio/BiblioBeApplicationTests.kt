5239c556559cc4af476d07ca5e6d5d5b46f9f545
Gilles <ggardet@hotmail.fr>
2020-05-19 16:29:06 +0200

    - use mongo container for the db - add dockerfile to deploy the jar

--- src/test/kotlin/com/ggardet/biblio/BiblioBeApplicationTests.kt
+++ src/test/kotlin/com/ggardet/biblio/BiblioBeApplicationTests.kt
@@ -1,13 +1,15 @@
 package com.ggardet.biblio
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [BiblioBeApplication::class])
 class BiblioApplicationTests {
 
     @Test
     fun contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
